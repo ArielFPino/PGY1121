b0c21f750f75549aa0ec7a0953ca2ff750668ed5
ArielFPino <134543060+ArielFPino@users.noreply.github.com>
2023-05-24 14:34:22 -0400

    datos.py v1

---
+++ Clase-12/datos.py
@@ -0,0 +1,9 @@
+#Ejemplo uso de repositorio
+print("Datos personales")
+print("-------------------")
+nom=str(input("Ingrese su nombre: "))
+edad=int(input("Ingrese su edad: "))
+print("-----------------------------")
+print(f"Su nombre es: {nom} y tiene {edad} años.")
+
+print("Programa finalizado./n")
